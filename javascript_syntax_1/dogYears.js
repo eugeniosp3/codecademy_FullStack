4ce2289a84fc1d668ba90e57e0309264534924c8
Gino P <64187887+eugeniosp3@users.noreply.github.com>
2021-05-12 00:34:30 -0400

    Delete dogYears.js

--- javascript_syntax_1/dogYears.js
+++
@@ -1,8 +0,0 @@
-myAge = 62; 
-firstTwo = 2 * 10.5
-lastYears = (myAge - 2) * 4
-dogYears = firstTwo + lastYears 
-console.log(dogYears)
-// my name in lower case
-myName = "Eugenio".toLowerCase()
-console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${dogYears} years old in dog years.`)
