597d93dfc5466b29a440f27685c51a3aea7f52b7
Gino P <64187887+eugeniosp3@users.noreply.github.com>
2021-05-12 00:33:04 -0400

    Add files via upload
    Tutorial project completion

---
+++ javascript_syntax_1/raceNumber.js
@@ -0,0 +1,5 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+earlyRegistration = false
+runnerAge = 44
+adultOrChild = ''
+
---
+++ javascript_syntax_1/raceNumber2.js
@@ -0,0 +1,30 @@
+// let raceNumber = Math.floor(Math.random() * 1000);
+// earlyRegistration = false
+// runnerAge = 44
+// adultOrChild = ''
+
+// if (runnerAge > 18 && earlyRegistration) {
+//   adultOrChild = 'Adult'
+//   raceNumber += 1000
+// }
+// else {
+//     console.log('Go to registration desk!')
+// }
+
+// if (runnerAge > 18 && earlyRegistration) {
+//   console.log( `Runner number ${raceNumber}, you will be running at 9:30AM!`) 
+//   }
+//   else if (runnerAge > 18 && !earlyRegistration) {
+//     console.log(`Runner number ${raceNumber}, you will be running at 11:00AM.`)
+
+//   }
+//   else {
+//     console.log('Go to registration desk!')
+// }
+
+// if (runnerAge < 18) {
+//   console.log(`Youth registrant, you will be running at 12:30 PM with Runner Number ${raceNumber}.`)
+// }
+// else {
+//     console.log('Go to registration desk!')
+// }
